4395b5b6c6c586146aab6f64bc2e8750a4a301b2
Hanno Braun <hanno@habraun.net>
2009-10-05 19:39:58 +0200

    Added the class VelocityConstraintSolver which can solve velocity constraints.

---
+++ src/main/scala/net/habraun/sd/dynamics/VelocityConstraintSolver.scala
@@ -0,0 +1,36 @@
+/*
+	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+
+package net.habraun.sd.dynamics
+
+
+
+class VelocityConstraintSolver {
+
+	def solve( constraints: Set[VelocityConstraint] ) {
+		for ( constraint <- constraints ) {
+			// Check if the actual velocity is greater than the maximum velocity.
+			if ( constraint.velocity.squaredLength > constraint.maximumVelocity * constraint.maximumVelocity ) {
+				// It is. Set the velocity to the maximum velocity while retaining the direction of the new
+				// velocity.
+				constraint.velocity = constraint.velocity * ( constraint.maximumVelocity / Math.sqrt(
+						constraint.velocity * constraint.velocity ) )
+			}
+		}
+	}
+}
---
+++ src/test/scala/net/habraun/sd/dynamics/VelocityConstraintSolverSpec.scala
@@ -0,0 +1,63 @@
+/*
+	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+
+package net.habraun.sd.dynamics
+
+
+
+import core.Body
+import math.Vec2D
+
+import scala.collection.immutable.HashSet
+
+import org.specs.Specification
+import org.specs.runner.JUnit4
+
+
+
+class VelocityConstraintSolverTest extends JUnit4( VelocityConstraintSolverSpec )
+
+
+
+object VelocityConstraintSolverSpec extends Specification {
+
+	"VelocityConstraintSolver" should {
+		"not modify the velocity of a passed Body if it is below the maximum velocity." in {
+			val solver = new VelocityConstraintSolver
+
+			val velocity = Vec2D( 3, 0 )
+			val body = new Body with VelocityConstraint {}
+			body.velocity = velocity
+			body.maximumVelocity = 5
+			solver.solve( HashSet( body ) )
+
+			body.velocity must beEqualTo( velocity )
+		}
+
+		"set the velocity to the maximum velocity if it is above maximum velocity." in {
+			val solver = new VelocityConstraintSolver
+
+			val body = new Body with VelocityConstraint {}
+			body.velocity = Vec2D( 6, 0 )
+			body.maximumVelocity = 5
+			solver.solve( HashSet( body ) )
+
+			body.velocity must beEqualTo( Vec2D( 5, 0 ) )
+		}
+	}
+}
