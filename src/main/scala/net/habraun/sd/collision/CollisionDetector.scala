4c1c46025d8e683cd7fbf50245119034c83ef4a5
Hanno Braun <hanno@habraun.net>
2009-10-06 22:16:53 +0200

    Added CollisionDetector, a StepPhase which performs collision detection by calling the phases.

---
+++ src/main/scala/net/habraun/sd/collision/CollisionDetector.scala
@@ -0,0 +1,48 @@
+/*
+	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+
+package net.habraun.sd.collision
+
+
+
+import core.StepPhase
+import phase.BroadPhase
+import phase.NarrowPhase
+import shape.Shape
+
+
+
+class CollisionDetector( broadPhase: BroadPhase, narrowPhase: NarrowPhase ) extends StepPhase[Shape] {
+
+	def step( dt: Double, shapes: Iterable[Shape] ) {
+		// Broad phase. Checks all shapes in a performant way and returns a list of possible collisions.
+		val possiblyCollidingPairs = broadPhase( shapes.toList )
+
+		// Narrow Phase. Performs a detailed and possibly performance-heavy collision check for a pair of
+		// shapes.
+		val possibleCollisions = for ( possibleCollision <- possiblyCollidingPairs ) yield {
+			narrowPhase( possibleCollision._1, possibleCollision._2 )
+		}
+
+		// Add all detected contacts to the shapes they belong to.
+		for ( possibleCollision <- possibleCollisions; collision <- possibleCollision ) {
+			collision.contact1.s.addContact( collision.contact1 )
+			collision.contact2.s.addContact( collision.contact2 )
+		}
+	}
+}
---
+++ src/test/scala/net/habraun/sd/collision/CollisionDetectorSpec.scala
@@ -0,0 +1,113 @@
+/*
+	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+
+package net.habraun.sd.collision
+
+
+
+import core.StepPhase
+import math.Vec2D
+import phase.BroadPhase
+import phase.Collision
+import phase.NarrowPhase
+import shape.Contact
+import shape.Shape
+
+import org.specs.Specification
+import org.specs.mock.Mockito
+import org.specs.runner.JUnit4
+
+
+
+class CollisionDetectorTest extends JUnit4( CollisionDetectorSpec )
+
+
+
+
+object CollisionDetectorSpec extends Specification with Mockito {
+
+	"CollisionDetector" should {
+		"take the collision phases as parameters." in {
+			val broadPhase = mock[BroadPhase]
+			val narrowPhase = mock[NarrowPhase]
+
+			new CollisionDetector( broadPhase, narrowPhase )
+		}
+
+		"be a StepPhase." in {
+			val broadPhase = mock[BroadPhase]
+			val narrowPhase = mock[NarrowPhase]
+			val detector = new CollisionDetector( broadPhase, narrowPhase )
+
+			detector must haveSuperClass[StepPhase[Shape]]
+		}
+
+		"pass a list of all objects to the broad phase." in {
+			val broadPhase = mock[BroadPhase]
+			val narrowPhase = mock[NarrowPhase]
+			val detector = new CollisionDetector( broadPhase, narrowPhase )
+			
+			val shape1 = mock[Shape]
+			val shape2 = mock[Shape]
+			val shapes = shape1::shape2::Nil
+
+			broadPhase( shape1::shape2::Nil ) returns Nil
+
+			detector.filterAndStep( 0.0, shapes )
+
+			broadPhase( shapes ) was called
+		}
+
+		"pass the pairs returned by the broad phase to the narrow phase." in {
+			val broadPhase = mock[BroadPhase]
+			val narrowPhase = mock[NarrowPhase]
+			val detector = new CollisionDetector( broadPhase, narrowPhase )
+
+			val shape1 = mock[Shape]
+			val shape2 = mock[Shape]
+			val shapes = shape1::shape2::Nil
+
+			broadPhase( shapes ) returns ( shape1, shape2 )::Nil
+			narrowPhase( shape1, shape2 ) returns None
+			
+			detector.filterAndStep( 0.0, shapes )
+
+			narrowPhase( shape1, shape2 ) was called
+		}
+
+		"add the contacts returned by the narrow phase to the shape." in {
+			val broadPhase = mock[BroadPhase]
+			val narrowPhase = mock[NarrowPhase]
+			val detector = new CollisionDetector( broadPhase, narrowPhase )
+
+			val shape1 = mock[Shape]
+			val shape2 = mock[Shape]
+			val shapes = shape1::shape2::Nil
+			val contact1 = Contact(shape1, Vec2D( 4, 4 ), Vec2D( 1, 0), shape2 )
+			val contact2 = Contact(shape2, Vec2D( 4, 4 ), Vec2D( -1, 0), shape1 )
+
+			broadPhase( shapes ) returns ( shape1, shape2 )::Nil
+			narrowPhase( shape1, shape2 ) returns Some( Collision( 0.5, contact1, contact2 ) )
+
+			detector.filterAndStep( 0.0, shapes )
+
+			shape1.addContact( contact1 ) was called
+			shape2.addContact( contact2 ) was called
+		}
+	}
+}
