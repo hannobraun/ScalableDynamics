03edaf34c223699d2aeef62d853227ebb32f08f2
Hanno Braun <hanno@habraun.net>
2009-11-27 14:23:12 +0100

    Did some refactorings and comment updates in ContinuousCircleLineSegmentTest.

--- src/main/scala/net/habraun/sd/collision/test/ContinuousCircleLineSegmentTest.scala
+++ src/main/scala/net/habraun/sd/collision/test/ContinuousCircleLineSegmentTest.scala
@@ -41,82 +41,89 @@ class ContinuousCircleLineSegmentTest extends CircleLineSegmentTest {
 
 	def apply( circle: Circle, lineSegment: LineSegment ): Option[ Contact ] = {
 
-		// As a first step, we compute the position vector of the point on the line segment with the smallest distance to the circle
-		// center. In other words, of all points that lie on the line segment, we compute the nearest to the circle center.
-		// To do this, we first need to compute a few values to help us.
-
 		// Let's save the shape attributes into short variables, so we have them handy for the long equations.
 		val pc = circle.previousPosition
-		val r = circle.radius
+		val rc = circle.radius
 		val pls = lineSegment.previousPosition
 		val dls = lineSegment.d
 
-		// We'll also need the relative velocity between the two shapes. We always treat the circle as moving and the line segment as
-		// standing still.
+		// We'll also need the velocities of the shapes and their relative velocity. We always treat the circle as moving and the line
+		// segment as standing still.
 		val vc = ( circle.position - circle.previousPosition )
 		val vls = ( lineSegment.position - lineSegment.previousPosition )
 		val v =  vc - vls
 
-		// The vector that points from the line segment position to the circle center.
-		val lsPosToCPos = pc - pls
+		// The general approach here is to sort out all the edge cases first. If none of them applies, we can compute the contact with the
+		// most general formula.
+		// First step: Does the circle intersect the line segment at the beginning of the movement?
+
+
+		// The vector that points from the line segment's position (one of the endpoints) to the circle center.
+		val lsPositionToCircleCenter = pc - pls
 
 		// The vector that points from the line segment position to the nearest point between the circle center and the line defined by the
 		// line segment.
-		val lsPosToNearestPointOnLine = lsPosToCPos.projectOn( dls )
+		val lsPositionToNearestPointOnLine = lsPositionToCircleCenter.projectOn( dls )
 
 		// The factor that needs to applied to the line segment's direction in order to get the vector that points from the beginning of
 		// the line segment to the nearest point between the line defined by the segment and the circle center.
-		val f = dls.computeFactorFor( lsPosToNearestPointOnLine )
+		val s = dls.computeFactorFor( lsPositionToNearestPointOnLine )
 
-		// Since the line segment and the factor define a point on a line, we can just trim that point to the line segment.
-		val nearestPointOnSegment = trimPointToLineSegment( pls, dls, f )
+		// We're interested in the closest point to the circle center that lies on the line segment. The line segments position, direction
+		// and the factor s already give us the closest point on the line. We just have to trim it to the line segment.
+		val closestPointOnSegment = trimPointToLineSegment( pls, dls, s )
 
-		// The next step is to determine if the circle and the line segment overlap initially. To do this, we first need to compute the
-		// vector that points from the circle center to the nearest point on the segment and its length (the distance between the circle
-		// center and the segment).
-		val circleCenterToNearestPointOnLs = nearestPointOnSegment - pc
-		val lsDistanceToCircleCenter = ( circleCenterToNearestPointOnLs ).length
+		// Now we can determine if the circle and the line segment overlap. To do this, we first need to compute the vector that points
+		// from the circle center to the closest point on the segment and its length (the distance between the circle center and the
+		// segment).
+		val circleCenterToClosestPointOnLs = closestPointOnSegment - pc
+		val lsDistanceToCircleCenter = ( circleCenterToClosestPointOnLs ).length
 
-		// Do they overlap initially?
-		if ( lsDistanceToCircleCenter <= r ) {
+		// So, do the circle and the line segment overlap initially?
+		if ( lsDistanceToCircleCenter <= rc ) {
 			// Yes they do. We define the point of contact as the nearest point we computed before. The rest is pretty obvious from there
 			// on.
-			val point = nearestPointOnSegment
-			val normal = circleCenterToNearestPointOnLs.normalize
+			val point = closestPointOnSegment
+			val normal = circleCenterToClosestPointOnLs.normalize
 			val depth = circle.radius - lsDistanceToCircleCenter
 			val t = 0.0
 
 			Some ( Contact( circle, lineSegment, point, normal, depth, t ) )
 		}
 		else if ( v == ZeroVector ) {
-			// They don't overlap initially and they also don't move.
+			// No, they don't. They also don't move relative to each other. This means there can be no contact.
 			None
 		}
 		else if ( !v.isLinearlyIndependentFrom( dls ) ) {
-			// They move, but the relative movement is parallel to the line segment.
+			// They don't intersect intially and they they move, but it's a special kind of movement: The relative movement of the circle
+			// is parallel to the direction of the line segment.
 
 			// Check which endpoint is the nearest to the initial position of the circle center.
 			val ep1 = pls
 			val ep2 = pls + dls
 			val ep = if ( ( pc - ep1 ).squaredLength < ( pc - ep2 ).squaredLength ) ep1 else ep2
 
-			// Compute the t for the moment the distance from the nearest endpoint is equal to the radius.
+			// Compute the t for the moment the distance from the nearest endpoint is equal to the radius. This can be done by solving the
+			// equation "| pc + t * v - ep | = rc".
 			val a = v.x * v.x + v.y * v.y
 			val b = 2 * pc.x * v.x + 2 * pc.y * v.y - 2 * v.x * ep.x - 2 * v.y * ep.y
-			val c = pc.x * pc.x + pc.y * pc.y + ep.x * ep.x + ep.y * ep.y - 2 * pc.x * ep.x - 2 * pc.y * ep.y - r * r
+			val c = pc.x * pc.x + pc.y * pc.y + ep.x * ep.x + ep.y * ep.y - 2 * pc.x * ep.x - 2 * pc.y * ep.y - rc * rc
 			val x = solveQuadraticEquation( a, b, c )
 			if ( x == Nil ) {
-				// No real solution.
+				// No real solution, which means that the circle never reaches the endpoint.
 				None
 			}
 			else {
-				// One or two real solutions. Doesn't matter, we're only interested in the earlier time anyway.
+				// One or two real solutions. Doesn't matter, we're only interested in the earlier time anyway, which is the smaller
+				// solution.
 				val t = x( 0 )
 
+				// That there's a solution doesn't mean they collide within the timeframe we're looking at. The circle might not move far
+				// enough or even in the wrong direction.
 				if ( t >= 0 && t <= 1.0 ) {
-
+					// Looks like they do collide. All that's left to do is to compute the contact attributes.
 					val normal = ( ep + t * vls - ( pc + t * vc ) ).normalize
-					val point = pc + t * vc + normal * r
+					val point = pc + t * vc + normal * rc
 					val depth = ( ( 1.0 - t ) * v ).length
 
 					Some( Contact( circle, lineSegment, point, normal, depth, t ) )
@@ -137,7 +144,7 @@ class ContinuousCircleLineSegmentTest extends CircleLineSegmentTest {
 			// the line defined by the line segment and points from the point where the circle would touch the line to the center of the
 			// line segment.
 			val dist = {
-				val lineOrthogonal = dls.orthogonal.normalize * r
+				val lineOrthogonal = dls.orthogonal.normalize * rc
 				if ( lineOrthogonal * ( pc - pls ) >= 0 ) lineOrthogonal else -lineOrthogonal
 			}
 
@@ -160,7 +167,7 @@ class ContinuousCircleLineSegmentTest extends CircleLineSegmentTest {
 				// The time of contact can be computed using a quadratic euqation.
 				val a = v.x * v.x + v.y * v.y
 				val b = 2 * v.x * pc.x + 2 * v.y * pc.y - 2 * v.x * p.x - 2 * v.y * p.y
-				val c = pc.x * pc.x + pc.y * pc.y + p.x * p.x + p.y * p.y - 2 * pc.x * p.x - 2 * pc.y * p.y - r * r
+				val c = pc.x * pc.x + pc.y * pc.y + p.x * p.x + p.y * p.y - 2 * pc.x * p.x - 2 * pc.y * p.y - rc * rc
 				val x = solveQuadraticEquation( a, b, c )
 				if (  x == Nil ) {
 					// No real solution.
@@ -170,7 +177,7 @@ class ContinuousCircleLineSegmentTest extends CircleLineSegmentTest {
 					// One or two real solution. Doesn't matter, we're only interested in the earlier time anyway.
 					val tls = x( 0 )
 					val normal = ( p + tls * vls - ( pc + tls * vc ) ).normalize
-					val point = pc + tls * vc + normal * r
+					val point = pc + tls * vc + normal * rc
 
 					Some( Contact( circle, lineSegment, point, normal, 0, tls ) )
 				}
