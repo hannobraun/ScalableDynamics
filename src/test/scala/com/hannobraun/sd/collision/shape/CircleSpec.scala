cbf4f7a99b92083c7d0768115cc41ac81ad2ac8e
Hanno Braun <mail@hannobraun.com>
2010-08-03 19:50:40 +0200

    Adapted copyright header.

--- src/test/scala/com/hannobraun/sd/collision/shape/CircleSpec.scala
+++ src/test/scala/com/hannobraun/sd/collision/shape/CircleSpec.scala
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2009 Hanno Braun <hanno@habraun.net>
+	Copyright (c) 2009, 2010 Hanno Braun <mail@hannobraun.com>
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
